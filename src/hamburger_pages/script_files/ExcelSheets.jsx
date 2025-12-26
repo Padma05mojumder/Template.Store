d40c14a997d42fe2981260043971e6f6c07fd2f8
Padmanava Mojumder <mojumderpadmanava@gmail.com>
2025-12-26 10:25:46 +0530

    dist updated

--- src/hamburger_pages/script_files/ExcelSheets.jsx
+++ src/hamburger_pages/script_files/ExcelSheets.jsx
@@ -7,22 +7,22 @@ section.render(
     <>
           <Word
               link="/src/pages/excel_pages/business1.html"
-              img="/src/assets/images/excel_images/business1.jpg"
+              img="/assets/images/excel_images/business1.jpg"
               name="Business and Strategic Analysis template 1"
           />
           <Word
               link="/src/pages/excel_pages/business2.html"
-              img="/src/assets/images/excel_images/business2.jpg"
+              img="/assets/images/excel_images/business2.jpg"
               name="Business and Strategic Analysis template 2"
           />
           <Word
               link="/src/pages/excel_pages/business3.html"
-              img="/src/assets/images/excel_images/business3.jpg"
+              img="/assets/images/excel_images/business3.jpg"
               name="Business and Strategic Analysis template 3"
           />
           <Word
               link="/src/pages/excel_pages/business4.html"
-              img="/src/assets/images/excel_images/business4.jpg"
+              img="/assets/images/excel_images/business4.jpg"
               name="Business and Strategic Analysis template 4"
           />  
       </>
@@ -32,22 +32,22 @@ section.render(
     <>
            <Word
                link="/src/pages/excel_pages/finance1.html"
-               img="/src/assets/images/excel_images/finance1.jpg"
+               img="/assets/images/excel_images/finance1.jpg"
                name="Finance and Accounting template 1"
            />
            <Word
                link="/src/pages/excel_pages/finance2.html"
-               img="/src/assets/images/excel_images/finance2.jpg"
+               img="/assets/images/excel_images/finance2.jpg"
                name="Finance and Accounting template 2"
            />
            <Word
                link="/src/pages/excel_pages/finance3.html"
-               img="/src/assets/images/excel_images/finance3.jpg"
+               img="/assets/images/excel_images/finance3.jpg"
                name="Finance and Accounting template 3"
            />
            <Word
                link="/src/pages/excel_pages/finance4.html"
-               img="/src/assets/images/excel_images/finance4.jpg"
+               img="/assets/images/excel_images/finance4.jpg"
                name="Finance and Accounting template 4"
            />   
        </>
@@ -57,22 +57,22 @@ section.render(
     <>
            <Word
                link="/src/pages/excel_pages/market1.html"
-               img="/src/assets/images/excel_images/market1.jpg"
+               img="/assets/images/excel_images/market1.jpg"
                name="Market and Research Analysis template 1"
            />
            <Word
                link="/src/pages/excel_pages/market2.html"
-               img="/src/assets/images/excel_images/market2.jpg"
+               img="/assets/images/excel_images/market2.jpg"
                name="Market and Research Analysis template 2"
            />
            <Word
                link="/src/pages/excel_pages/market3.html"
-               img="/src/assets/images/excel_images/market3.jpg"
+               img="/assets/images/excel_images/market3.jpg"
                name="Market and Research Analysis template 3"
            />
            <Word
                link="/src/pages/excel_pages/market4.html"
-               img="/src/assets/images/excel_images/market4.jpg"
+               img="/assets/images/excel_images/market4.jpg"
                name="Market and Research Analysis template 4"
            />  
        </>
@@ -82,22 +82,22 @@ section.render(
      <>
            <Word
                link="/src/pages/excel_pages/marks1.html"
-               img="/src/assets/images/excel_images/marks1.jpg"
+               img="/assets/images/excel_images/marks1.jpg"
                name="Marksheet template 1"
            />
            <Word
                link="/src/pages/excel_pages/marks2.html"
-               img="/src/assets/images/excel_images/marks2.jpg"
+               img="/assets/images/excel_images/marks2.jpg"
                name="Marksheet template 2"
            />
            <Word
                link="/src/pages/excel_pages/marks3.html"
-               img="/src/assets/images/excel_images/marks3.jpg"
+               img="/assets/images/excel_images/marks3.jpg"
                name="Marksheet template 3"
            />
            <Word
                link="/src/pages/excel_pages/marks4.html"
-               img="/src/assets/images/excel_images/marks4.jpg"
+               img="/assets/images/excel_images/marks4.jpg"
                name="Marksheet template 4"
            />
        </>
@@ -107,22 +107,22 @@ section.render(
     <>
             <Word
                 link="/src/pages/excel_pages/operation1.html"
-                img="/src/assets/images/excel_images/operation1.jpg"
+                img="/assets/images/excel_images/operation1.jpg"
                 name="Operation Management template 1"
             />
             <Word
                 link="/src/pages/excel_pages/operation2.html"
-                img="/src/assets/images/excel_images/operation2.jpg"
+                img="/assets/images/excel_images/operation2.jpg"
                 name="Operation Management template 2"
             />
             <Word
                 link="/src/pages/excel_pages/operation3.html"
-                img="/src/assets/images/excel_images/operation3.jpg"
+                img="/assets/images/excel_images/operation3.jpg"
                 name="Operation Management template 3"
             />
             <Word
                 link="/src/pages/excel_pages/operation4.html"
-                img="/src/assets/images/excel_images/operation4.jpg"
+                img="/assets/images/excel_images/operation4.jpg"
                 name="Operation Management template 4"
             /> 
         </>
@@ -132,22 +132,22 @@ section.render(
    <>
            <Word
                link="/src/pages/excel_pages/performance1.html"
-               img="/src/assets/images/excel_images/performance1.jpg"
+               img="/assets/images/excel_images/performance1.jpg"
                name="Performance Reporting template 1"
            />
            <Word
                link="/src/pages/excel_pages/performance2.html"
-               img="/src/assets/images/excel_images/performance2.jpg"
+               img="/assets/images/excel_images/performance2.jpg"
                name="Performance Reporting template 2"
            />
            <Word
                link="/src/pages/excel_pages/performance3.html"
-               img="/src/assets/images/excel_images/performance3.jpg"
+               img="/assets/images/excel_images/performance3.jpg"
                name="Performance Reporting template 3"
            />
            <Word
                link="/src/pages/excel_pages/performance4.html"
-               img="/src/assets/images/excel_images/performance4.jpg"
+               img="/assets/images/excel_images/performance4.jpg"
                name="Performance Reporting template 4"
            />
        </>
@@ -157,22 +157,22 @@ section.render(
     <>
             <Word
                 link="/src/pages/excel_pages/project1.html"
-                img="/src/assets/images/excel_images/project1.jpg"
+                img="/assets/images/excel_images/project1.jpg"
                 name="Project Management template 1"
             />
             <Word
                 link="/src/pages/excel_pages/project2.html"
-                img="/src/assets/images/excel_images/project2.jpg"
+                img="/assets/images/excel_images/project2.jpg"
                 name="Project Management template 2"
             />
             <Word
                 link="/src/pages/excel_pages/project3.html"
-                img="/src/assets/images/excel_images/project3.jpg"
+                img="/assets/images/excel_images/project3.jpg"
                 name="Project Management template 3"
             />
             <Word
                 link="/src/pages/excel_pages/project4.html"
-                img="/src/assets/images/excel_images/project4.jpg"
+                img="/assets/images/excel_images/project4.jpg"
                 name="Project Management template 4"
             />  
         </>
@@ -182,22 +182,22 @@ section.render(
     <>
            <Word
                link="/src/pages/excel_pages/resource1.html"
-               img="/src/assets/images/excel_images/resource1.png"
+               img="/assets/images/excel_images/resource1.png"
                name="Human Resources template 1"
            />
            <Word
                link="/src/pages/excel_pages/resource2.html"
-               img="/src/assets/images/excel_images/resource2.jpg"
+               img="/assets/images/excel_images/resource2.jpg"
                name="Human Resources template 2"
            />
            <Word
                link="/src/pages/excel_pages/resource3.html"
-               img="/src/assets/images/excel_images/resource3.jpg"
+               img="/assets/images/excel_images/resource3.jpg"
                name="Human Resources template 3"
            />
            <Word
                link="/src/pages/excel_pages/resource4.html"
-               img="/src/assets/images/excel_images/resource4.jpg"
+               img="/assets/images/excel_images/resource4.jpg"
                name="Human Resources template 4"
            /> 
        </>
@@ -207,22 +207,22 @@ section.render(
     <>
         <Word
             link="/src/pages/excel_pages/statistics1.html"
-            img="/src/assets/images/excel_images/statistics1.jpg"
+            img="/assets/images/excel_images/statistics1.jpg"
             name="Statistical Analysis template 1"
         />
            <Word
               link="/src/pages/excel_pages/statistics2.html"
-               img="/src/assets/images/excel_images/statistics2.jpg"
+               img="/assets/images/excel_images/statistics2.jpg"
                name="Statistical Analysis template 2"
            />
            <Word
                link="/src/pages/excel_pages/statistics3.html"
-               img="/src/assets/images/excel_images/statistics3.jpg"
+               img="/assets/images/excel_images/statistics3.jpg"
                name="Statistical Analysis template 3"
            />
            <Word
                link="/src/pages/excel_pages/statistics4.html"
-               img="/src/assets/images/excel_images/statistics4.jpg"
+               img="/assets/images/excel_images/statistics4.jpg"
                name="Statistical Analysis template 4"
            />
        </>
--- src/hamburger_pages/script_files/PptSlides.jsx
+++ src/hamburger_pages/script_files/PptSlides.jsx
@@ -7,22 +7,22 @@ section.render(
     <>
         <Word
             link="/src/pages/ppt_pages/data1.html"
-            img="/src/assets/images/ppt_images/data12/data1.jpg"
+            img="/assets/images/ppt_images/data12/data1.jpg"
             name="Data Visualization template 1"
         />
         <Word
             link="/src/pages/ppt_pages/data2.html"
-            img="/src/assets/images/ppt_images/data22/data2.jpg"
+            img="/assets/images/ppt_images/data22/data2.jpg"
             name="Data Visualization template 2"
         />
         <Word
             link="/src/pages/ppt_pages/data3.html"
-            img="/src/assets/images/ppt_images/data32/data3.jpg"
+            img="/assets/images/ppt_images/data32/data3.jpg"
             name="Data Visualization template 3"
         />
         <Word
             link="/src/pages/ppt_pages/data4.html"
-            img="/src/assets/images/ppt_images/data42/data4.jpg"
+            img="/assets/images/ppt_images/data42/data4.jpg"
             name="Data Visualization template 4"
         />  
     </>
@@ -32,22 +32,22 @@ section.render(
   <>
         <Word
             link="/src/pages/ppt_pages/info1.html"
-            img="/src/assets/images/ppt_images/info12/info1.jpg"
+            img="/assets/images/ppt_images/info12/info1.jpg"
             name="Infographics Template 1"
         />
         <Word
             link="/src/pages/ppt_pages/info2.html"
-            img="/src/assets/images/ppt_images/info22/info2.jpg"
+            img="/assets/images/ppt_images/info22/info2.jpg"
             name="Infographics Template 2"
         />
         <Word
             link="/src/pages/ppt_pages/info3.html"
-            img="/src/assets/images/ppt_images/info32/info3.jpg"
+            img="/assets/images/ppt_images/info32/info3.jpg"
             name="Infographics Template 3"
         />
         <Word
             link="/src/pages/ppt_pages/info4.html"
-            img="/src/assets/images/ppt_images/info42/info4.jpg"
+            img="/assets/images/ppt_images/info42/info4.jpg"
             name="Infographics Template 4"
         />   
     </>
@@ -57,22 +57,22 @@ section.render(
    <>
         <Word
             link="/src/pages/ppt_pages/lecture1.html"
-            img="/src/assets/images/ppt_images/lecture12/lecture1.jpg"
+            img="/assets/images/ppt_images/lecture12/lecture1.jpg"
             name="Lectures and Lesson Template 1"
         />
         <Word
             link="/src/pages/ppt_pages/lecture2.html"
-            img="/src/assets/images/ppt_images/lecture22/lecture2.jpg"
+            img="/assets/images/ppt_images/lecture22/lecture2.jpg"
             name="Lectures and Lesson Template 2"
         />
         <Word
             link="/src/pages/ppt_pages/lecture3.html"
-            img="/src/assets/images/ppt_images/lecture32/lecture3.jpg"
+            img="/assets/images/ppt_images/lecture32/lecture3.jpg"
             name="Lectures and Lesson Template 3"
         />
         <Word
             link="/src/pages/ppt_pages/lecture4.html"
-            img="/src/assets/images/ppt_images/lecture42/lecture4.jpg"
+            img="/assets/images/ppt_images/lecture42/lecture4.jpg"
             name="Lectures and Lesson Template 4"
         />  
     </>
@@ -82,22 +82,22 @@ section.render(
        <>
            <Word
                link="/src/pages/ppt_pages/market1.html"
-               img="/src/assets/images/ppt_images/market12/market1.jpg"
+               img="/assets/images/ppt_images/market12/market1.jpg"
                name="Marketing and Sales Template 1"
            />
            <Word
                link="/src/pages/ppt_pages/market2.html"
-               img="/src/assets/images/ppt_images/market22/market2.jpg"
+               img="/assets/images/ppt_images/market22/market2.jpg"
                name="Marketing and Sales Template 2"
            />
            <Word
                link="/src/pages/ppt_pages/market3.html"
-               img="/src/assets/images/ppt_images/market32/market3.jpg"
+               img="/assets/images/ppt_images/market32/market3.jpg"
                name="Marketing and Sales Template 3"
            />
            <Word
                link="/src/pages/ppt_pages/market4.html"
-               img="/src/assets/images/ppt_images/market42/market4.jpg"
+               img="/assets/images/ppt_images/market42/market4.jpg"
                name="Marketing and Sales Template 4"
            />
        </>
@@ -107,22 +107,22 @@ section.render(
     <>
             <Word
                 link="/src/pages/ppt_pages/meeting1.html"
-                img="/src/assets/images/ppt_images/meeting12/meeting1.jpg"
+                img="/assets/images/ppt_images/meeting12/meeting1.jpg"
                 name="Corporate Meeting Template 1"
             />
             <Word
                 link="/src/pages/ppt_pages/meeting2.html"
-                img="/src/assets/images/ppt_images/meeting22/meeting2.jpg"
+                img="/assets/images/ppt_images/meeting22/meeting2.jpg"
                 name="Corporate Meeting Template 2"
             />
             <Word
                 link="/src/pages/ppt_pages/meeting3.html"
-                img="/src/assets/images/ppt_images/meeting32/meeting3.jpg"
+                img="/assets/images/ppt_images/meeting32/meeting3.jpg"
                 name="Corporate Meeting Template 3"
             />
             <Word
                 link="/src/pages/ppt_pages/meeting4.html"
-                img="/src/assets/images/ppt_images/meeting42/meeting4.jpg"
+                img="/assets/images/ppt_images/meeting42/meeting4.jpg"
                 name="Corporate Meeting Template 4"
             /> 
         </>
@@ -132,22 +132,22 @@ section.render(
    <>
            <Word
                link="/src/pages/ppt_pages/portfolio1.html"
-               img="/src/assets/images/ppt_images/portfolio12/portfolio1.jpg"
+               img="/assets/images/ppt_images/portfolio12/portfolio1.jpg"
                name="Digital Portfolio Template 1"
            />
            <Word
                link="/src/pages/ppt_pages/portfolio2.html"
-               img="/src/assets/images/ppt_images/portfolio22/portfolio2.jpg"
+               img="/assets/images/ppt_images/portfolio22/portfolio2.jpg"
                name="Digital Portfolio Template 2"
            />
            <Word
                link="/src/pages/ppt_pages/portfolio3.html"
-               img="/src/assets/images/ppt_images/portfolio32/portfolio3.jpg"
+               img="/assets/images/ppt_images/portfolio32/portfolio3.jpg"
                name="Digital Portfolio Template 3"
            />
            <Word
                link="/src/pages/ppt_pages/portfolio4.html"
-               img="/src/assets/images/ppt_images/portfolio42/portfolio4.jpg"
+               img="/assets/images/ppt_images/portfolio42/portfolio4.jpg"
                name="Digital Portfolio Template 4"
            />
        </>
@@ -157,22 +157,22 @@ section.render(
    <>
            <Word
                link="/src/pages/ppt_pages/project1.html"
-               img="/src/assets/images/ppt_images/project12/project1.jpg"
+               img="/assets/images/ppt_images/project12/project1.jpg"
                name="Project Management Template 1"
            />
            <Word
                link="/src/pages/ppt_pages/project2.html"
-               img="/src/assets/images/ppt_images/project22/project2.jpg"
+               img="/assets/images/ppt_images/project22/project2.jpg"
                name="Project Management Template 2"
            />
            <Word
                link="/src/pages/ppt_pages/project3.html"
-               img="/src/assets/images/ppt_images/project32/project3.jpg"
+               img="/assets/images/ppt_images/project32/project3.jpg"
                name="Project Management Template 3"
            />
            <Word
                link="/src/pages/ppt_pages/project4.html"
-               img="/src/assets/images/ppt_images/project42/project4.jpg"
+               img="/assets/images/ppt_images/project42/project4.jpg"
                name="Project Management Template 4"
            />  
        </>
@@ -182,22 +182,22 @@ section.render(
      <>
             <Word
                 link="/src/pages/ppt_pages/student1.html"
-                img="/src/assets/images/ppt_images/student12/student1.jpg"
+                img="/assets/images/ppt_images/student12/student1.jpg"
                 name="Student Assignment Template 1"
             />
             <Word
                 link="/src/pages/ppt_pages/student2.html"
-                img="/src/assets/images/ppt_images/student22/student2.jpg"
+                img="/assets/images/ppt_images/student22/student2.jpg"
                 name="Student Assignment Template 2"
             />
             <Word
                 link="/src/pages/ppt_pages/student3.html"
-                img="/src/assets/images/ppt_images/student32/student3.jpg"
+                img="/assets/images/ppt_images/student32/student3.jpg"
                 name="Student Assignment Template 3"
             />
             <Word
                 link="/src/pages/ppt_pages/student4.html"
-                img="/src/assets/images/ppt_images/student42/student4.jpg"
+                img="/assets/images/ppt_images/student42/student4.jpg"
                 name="Student Assignment Template 4"
             /> 
         </>
@@ -207,22 +207,22 @@ section.render(
     <>
            <Word
                link="/src/pages/ppt_pages/training1.html"
-               img="/src/assets/images/ppt_images/training12/training1.jpg"
+               img="/assets/images/ppt_images/training12/training1.jpg"
                name="Development Template 1"
            />
            <Word
                link="/src/pages/ppt_pages/training2.html"
-               img="/src/assets/images/ppt_images/training22/training2.jpg"
+               img="/assets/images/ppt_images/training22/training2.jpg"
                name="Development Template 2"
            />
            <Word
                link="/src/pages/ppt_pages/training3.html"
-               img="/src/assets/images/ppt_images/training32/training3.jpg"
+               img="/assets/images/ppt_images/training32/training3.jpg"
                name="Development Template 3"
            />
            <Word
                link="/src/pages/ppt_pages/training4.html"
-               img="/src/assets/images/ppt_images/training42/training4.jpg"
+               img="/assets/images/ppt_images/training42/training4.jpg"
                name="Development Template 4"
            />
        </>
